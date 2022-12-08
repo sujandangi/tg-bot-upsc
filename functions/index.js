24cf78a90e5fa0e01921b53de9e12e898288ae84
Sujan Singh Dangi <sujandangi@gmail.com>
2022-12-09 02:48:53 +0530

    Bot working as expected

--- functions/index.js
+++ functions/index.js
@@ -12,32 +12,60 @@ if (Object.keys(functions.config()).length) {
 // ------------------------handling OpenAI API----------------------------
 const client = axios.create({
   headers: {
-    Authorization: "Bearer " + config.service.openai_key,
+    Authorization:
+      "Bearer " + "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",
   },
 });
 
-const params = {
-  prompt: "tell me a motivating quote.",
-  max_tokens: 100,
-};
+// -------------------------------OpenAI API------------------------------------------
 
-let data = "please wait";
+// ------------------------Telegram Bot-------------------------------------------------
+const {Telegraf} = require("telegraf");
 
-client
-  .post(
-    "https://api.openai.com/v1/engines/text-davinci-003/completions",
-    params
-  )
-  .then((result) => {
-    console.log(result.data);
-    data = result.data.choices[0].text;
-  })
-  .catch((err) => {
-    console.log(err);
-    data = err;
-  });
+const bot = new Telegraf(config.service.telegram_key);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  // functions.logger.info("Hello logs!", { structuredData: true });
-  response.send(`Sent from openai >> ${data}`); //
+bot.start((ctx) =>
+  ctx.reply("Welcome! Type anything and you will receive a motivating quote!")
+);
+bot.on("text", async (ctx) => {
+  try {
+    const params = {
+      prompt: "tell me a motivating quote.",
+      max_tokens: 100,
+    };
+
+    const response = await client.post(
+      "https://api.openai.com/v1/engines/text-davinci-003/completions",
+      params
+    );
+    // const completion = response.data.choices[0].text;
+
+    return ctx.reply(response.data.choices[0].text);
+  } catch (error) {
+    return ctx.reply("error");
+  }
+  // client
+  // .post(
+  //   "https://api.openai.com/v1/engines/text-davinci-003/completions",
+  //   params
+  // )
+  // .then((result) => {
+  //   console.log(result.data);
+  //   data = result.data.choices[0].text;
+  //   await ctx.reply(`${data}`);
+  // })
+  // .catch((err) => {
+  //   console.log(err);
+  //   data = err;
+  //   await ctx.reply(`${data}`);
 });
+bot.launch();
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   // functions.logger.info("Hello logs!", { structuredData: true });
+//   response.send(`Sent from openai >>`); //
+// });
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
