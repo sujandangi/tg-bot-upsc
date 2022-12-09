506647af1c3d9738ed0b49e1fb4ed119d957f2cf
Sujan Singh Dangi <sujandangi@gmail.com>
2022-12-09 13:36:38 +0530

    Working in deploy. keys retracted

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,30 @@
-const mySecret = process.env["OPENAI_API_KEY"];
 const axios = require("axios");
 const functions = require("firebase-functions");
 
 // using environment variables
-let config = require("../env.json");
-// if present in function.config()? use them instead
-if (Object.keys(functions.config()).length) {
-  config = functions.config();
-}
+// let config = require("../env.json");
+// // if present in function.config()? use them instead
+// if (Object.keys(functions.config()).length) {
+//   config = functions.config();
+// }
 
-// ------------------------handling OpenAI API----------------------------
+// ------------------------handling OpenAI API-----------------
 const client = axios.create({
   headers: {
-    Authorization:
-      "Bearer " + config.service.openai_key,
+    Authorization: "Bearer " + "",
   },
 });
 
-// -------------------------------OpenAI API------------------------------------------
+// -------------------------------OpenAI API--------------------
 
-// ------------------------Telegram Bot-------------------------------------------------
+// ------------------------Telegram Bot------------------------
 const {Telegraf} = require("telegraf");
-const { message, editedMessage, channelPost, editedChannelPost, callbackQuery } = require("telegraf/filters");
+const {message} = require("telegraf/filters");
 
-const bot = new Telegraf(config.service.telegram_key);
+const bot = new Telegraf("");
 
 bot.start((ctx) =>
-  ctx.reply("Welcome! Type anything and you will receive a motivating quote!")
+  ctx.reply("Welcome! Type anything and you will receive a motivating quote!"),
 );
 bot.on(message("text"), async (ctx) => {
   try {
@@ -36,11 +34,9 @@ bot.on(message("text"), async (ctx) => {
     };
 
     const response = await client.post(
-      "https://api.openai.com/v1/engines/text-davinci-003/completions",
-      params
+        "https://api.openai.com/v1/engines/text-davinci-003/completions",
+        params,
     );
-    // const completion = response.data.choices[0].text;
-
     return ctx.reply(response.data.choices[0].text);
   } catch (error) {
     return ctx.reply("error");
@@ -49,9 +45,9 @@ bot.on(message("text"), async (ctx) => {
 bot.launch();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send(`Sent from openai >>`); //
+  response.send("Sent from openai >>"); //
 });
 
-// Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+// // Enable graceful stop
+// process.once("SIGINT", () => bot.stop("SIGINT"));
+// process.once("SIGTERM", () => bot.stop("SIGTERM"));
